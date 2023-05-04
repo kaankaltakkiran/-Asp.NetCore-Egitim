9831767dfbc3c2933105bd3d91b85c23bc2f502c
kaankalttakiran <durdu.kaan.kaltakkiran@gmail.com>
2023-05-04 15:48:50 +0300

    Cookie

---
+++ MyAspNetCoreApp.Web/Controllers/CookieController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult CookieCreate()
+        {
+            //Cookie ekleme
+            HttpContext.Response.Cookies.Append("background-color", "black",new CookieOptions() { Expires = DateTime.Now.AddDays(2), });
+            return View();
+        }
+        public IActionResult CookieRead() {
+            //Cookie Okuma
+            var bgcolor=HttpContext.Request.Cookies["background-color"];
+            ViewBag.BackColor = bgcolor;
+            return View();
+        }
+    }
+}
